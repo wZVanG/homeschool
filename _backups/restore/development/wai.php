7f07b9600e325ba2fb05e5698a86a294c4494ee7
Walter Chapilliquen <wz.vang@gmail.com>
2021-10-03 14:51:32 -0500

    update

---
+++ _backups/restore/development/wai.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$config["wai"] = [
+    "assets_version" => 1000,
+    "sede_unica" => false,
+    "is_localhost" => $_SERVER["HTTP_HOST"] === "localhost",
+    "mantenimiento" => false,
+    "ips_habilitadas" => [],
+    "gmaps_key" => "AIzaSyD_QsZtwcNzM7jH1A7FMfvDQVYqM2V70QU",
+    "assets_production" => false
+];
+
+?>
